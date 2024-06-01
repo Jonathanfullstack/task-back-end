71f3f9163e639ae63950ad8c51e9b12bdde3f36f
Jonathan Vinicius <jonathantotini@gmail.com>
2024-06-01 00:14:18 -0500

    feat fizemos uma task de delet

--- index.js
+++ index.js
@@ -24,7 +24,25 @@ app.post("/tasks", async (req, res) => {
         await newTask.save();
         res.status(201).send(newTask);
     } catch (error) {
-        res.status(501).send(error.message);
+        res.status(500).send(error.message);
+    }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+
+        const taskToDeleted = await TaskModel.findById(taskId);
+
+        if (!taskToDeleted) {
+            return res.status(500).send("Essa tarefa nao foi encontrada !");
+        }
+
+        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+        res.status(200).send(deletedTask);
+    } catch {
+        res.status(500).send(error.message);
     }
 });
 
