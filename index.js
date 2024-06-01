d0d43c5d33d4c81e4c8d4f293d68e878f0a47644
Jonathan Vinicius <jonathantotini@gmail.com>
2024-05-31 23:54:49 -0500

    feat commite task, newtask

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const connectToDatabase = require("./src/database/mongoose.database");
+const TaskModel = require("./src/models/task.models");
 dotenv.config();
+
 const app = express();
+app.use(express.json());
 connectToDatabase();
 
-app.get("/tasks", (req, res) => {
-    const tasks = [{ description: "estudar programacao", isCompleted: false }];
-    res.status(200).send(tasks);
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await TaskModel.find({});
+        res.status(200).send(tasks);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post("/tasks", async (req, res) => {
+    try {
+        const newTask = new TaskModel(req.body);
+        await newTask.save();
+        res.status(201).send(newTask);
+    } catch (error) {
+        res.status(501).send(error.message);
+    }
 });
 
 app.listen(8000, () => console.log("listen on port 8000"));
