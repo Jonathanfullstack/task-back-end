a471f6776d73a553c881a7d0b93246d3b99e2ba1
Jonathan Vinicius <jonathantotini@gmail.com>
2024-06-01 00:29:20 -0500

    feat adicionamos uma rota de mensagem dizendo que nao foi encontrada a task

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ app.get("/tasks", async (req, res) => {
     }
 });
 
+app.get("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+
+        const task = await TaskModel.findById(taskId);
+
+        if (!task) {
+            return res.status(404).send("essa tarefa nao foi encontrada!");
+        }
+        return res.status(200).send(task);
+    } catch {
+        res.status(500).send(error.message);
+    }
+});
+
 app.post("/tasks", async (req, res) => {
     try {
         const newTask = new TaskModel(req.body);
@@ -35,7 +50,7 @@ app.delete("/tasks/:id", async (req, res) => {
         const taskToDeleted = await TaskModel.findById(taskId);
 
         if (!taskToDeleted) {
-            return res.status(500).send("Essa tarefa nao foi encontrada !");
+            return res.status(404).send("Essa tarefa nao foi encontrada !");
         }
 
         const deletedTask = await TaskModel.findByIdAndDelete(taskId);
