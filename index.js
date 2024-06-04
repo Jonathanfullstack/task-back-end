96b2635e5d5fde871ad36dd4a4109b332ff1764e
Jonathan Vinicius <jonathantotini@gmail.com>
2024-06-04 09:23:19 -0500

    feat fizemso a conexao do banco de dados com o front

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const taskRouter = require("./src/routes/task.routes");
 
 const connectToDatabase = require("./src/database/mongoose.database");
@@ -8,6 +9,7 @@ const TaskModel = require("./src/models/task.models");
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 connectToDatabase();
 app.use("/tasks", taskRouter);
